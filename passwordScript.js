a7f1714c370468aa178509d26f74f930917ef6d4
Hubert <hubert129@vp.pl>
2024-01-20 13:11:29 +0100

    corrected password script

--- passwordScript.js
+++ passwordScript.js
@@ -4,15 +4,28 @@ function generateSecurePassword() {
     const numbers = '0123456789';
     const specialCharacters = '!@#$%^&*()-_+=<>?';
 
-    const allCharacters = lowercaseLetters + uppercaseLetters + numbers + specialCharacters;
+    const getRandomChar = (charSet) => {
+        const randomIndex = Math.floor(Math.random() * charSet.length);
+        return charSet[randomIndex];
+    };
 
-    let password = '';
-    while (password.length < 8) {
+    const password =
+        getRandomChar(lowercaseLetters) +
+        getRandomChar(uppercaseLetters) +
+        getRandomChar(numbers) +
+        getRandomChar(specialCharacters);
+
+    const remainingLength = 8 - password.length;
+    const allCharacters = lowercaseLetters + uppercaseLetters + numbers + specialCharacters;
+    
+    for (let i = 0; i < remainingLength; i++) {
         const randomIndex = Math.floor(Math.random() * allCharacters.length);
         password += allCharacters[randomIndex];
     }
 
-    return password;
+    const shuffledPassword = password.split('').sort(() => Math.random() - 0.5).join('');
+
+    return shuffledPassword;
 }
 
 const securePassword = generateSecurePassword();
